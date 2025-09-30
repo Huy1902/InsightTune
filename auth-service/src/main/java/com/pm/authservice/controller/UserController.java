244b3cd6d1c0800c3d187d6553fd2a3d3695a534
Dinh Tran <23020043@vnu.edu.vn>
2025-10-01 01:19:07 +0700

    change password api

--- auth-service/src/main/java/com/pm/authservice/controller/UserController.java
+++ auth-service/src/main/java/com/pm/authservice/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.pm.authservice.controller;
 
+import com.pm.authservice.dto.request.ChangePasswordRequest;
 import com.pm.authservice.dto.request.UpdateRoleRequest;
 import com.pm.authservice.dto.response.ApiResponse;
 import com.pm.authservice.service.UserService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/user")
@@ -21,10 +25,19 @@ public class UserController {
 
     @PutMapping("/updateRole")
     public ApiResponse<String> updateRole(@RequestBody UpdateRoleRequest updateRoleRequest) {
-        int count = userService.updateRole(updateRoleRequest);
+        userService.updateRole(updateRoleRequest);
         return ApiResponse.<String>builder()
                 .code(200)
-                .result("Number of records updated: " + count)
+                .result("Update role successful")
+                .build();
+    }
+
+    @PutMapping("/changePassword")
+    public ApiResponse<String> changePassword(@Valid @RequestBody ChangePasswordRequest updatePassword, Principal principal) {
+        userService.changePassword(updatePassword, principal.getName());
+        return ApiResponse.<String>builder()
+                .code(200)
+                .result("Change password successful")
                 .build();
     }
 }
